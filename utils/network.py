eab6afa3f08c5ad97d61272933c51f1adb97a910
Petr Houška <houskape@gmail.com>
2020-07-08 02:14:03 +0200

    Update utils (get network inputs, hidden prints).

--- utils/network.py
+++ utils/network.py
@@ -6,17 +6,10 @@ import NDN3.NDN as NDN
 
 from datetime import datetime
 
-def get_network(train_input, train_output, 
-                  larg, opt_params, hsm_params, noise_dist='poisson',
-                  test_input = None, test_output = None, 
-                  train_data_filters=None, test_data_filters=None,
-                  custom_name = None, 
-                  seed=0):
-
-    # The seeds within NDN are applied only on ._build_graph which happens after weights get initialized
-    # ..need to set it now -> when NDN gets created -> weight get initiated as part of ._define_network
-    np.random.seed(seed)
-    tf.set_random_seed(seed)
+def get_network_inputs(train_input, train_output,
+                        larg, opt_params,
+                        test_input = None, test_output = None,
+                        train_data_filters=None, test_data_filters=None, custom_name = None):
     
     time_str = datetime.now().strftime("%d-%m_%H-%M-%S")
     name_str = time_str + "_" + custom_name if custom_name is not None else time_str
@@ -33,12 +26,33 @@ def get_network(train_input, train_output,
         data_filters = train_data_filters
         opt_params['early_stop'] = 0 # If we don't have test data -> shouldn't be early stopping (could early stop on train)
         test_len = 0  
-        
+
     train_indxs = np.array(range(train_len))
     test_indxs = np.array(range(train_len, train_len + test_len)) if test_len > 0 else None
-    
+
+    return (input, output, train_indxs, test_indxs, data_filters, larg, opt_params, name_str)
+
+
+def get_network(train_input, train_output, 
+                  larg, opt_params, hsm_params, noise_dist='poisson',
+                  test_input = None, test_output = None, 
+                  train_data_filters=None, test_data_filters=None,
+                  custom_name = None, 
+                  seed=0):
+
+    # The seeds within NDN are applied only on ._build_graph which happens after weights get initialized
+    # ..need to set it now -> when NDN gets created -> weight get initiated as part of ._define_network
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+        
+    input_params = get_network_inputs(train_input, train_output, 
+                                        larg, opt_params, 
+                                        test_input, test_output, 
+                                        train_data_filters, test_data_filters, 
+                                        custom_name)
+
     hsm = NDN.NDN(hsm_params, noise_dist=noise_dist, tf_seed=seed)
-    return hsm, (input, output, train_indxs, test_indxs, data_filters, larg, opt_params, name_str)
+    return hsm, input_params
 
 def train_network(train_input, train_output, 
                   larg, opt_params, hsm_params, noise_dist='poisson',
