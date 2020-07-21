ff0e5ac1aec0766b5a34df7f79796e103ba6f1ec
Petr Houška <houskape@gmail.com>
2020-07-21 17:34:03 +0200

    Add exp_multi1/bs3_exp42.

---
+++ experiments/experiments_mult1/bs3_exp42_runner.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import utils.runners as urun
+
+if __name__ == "__main__":
+    exp_folder = "experiments_mult1"
+    exp = "bs3_exp4"
+    exp_rev = "2"
+    runner = urun.get_runner(sys.argv[1])
+    run = 0
+    runner(
+        exp_folder, exp,
+        f"--exp_folder={exp_folder} --exp={exp}{exp_rev} --run={run} --hidden={0.2} --dog_layers={9} --regions={str([3]).replace(' ','')}", 
+        run
+        )
+    run += 1
+
+    for hidden in [0.2, 0.4]:
+        for dog_layer in [9, 15, 25]:
+            for regions in [[2, 3]]:
+                runner(
+                    exp_folder, exp,
+                    f"--exp_folder={exp_folder} --exp={exp}{exp_rev} --run={run} --hidden={hidden} --dog_layers={dog_layer} --regions={str(regions).replace(' ','')}", 
+                    run
+                    )
+                run += 1
