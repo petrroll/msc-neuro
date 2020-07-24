ad6147a6b23655ee8a1d0df2c35399c051ca2c11
Petr Houška <houskape@gmail.com>
2020-07-25 00:15:16 +0200

    Add exp2/bs3_exp10.

---
+++ experiments/experiments_2/bs3_exp10_runner.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import utils.runners as urun
+
+if __name__ == "__main__":
+    exp_folder = "experiments_2"
+    exp = "bs3_exp10"
+    runner = urun.get_runner(sys.argv[1])
+    run = 0
+    for lin_scale in [True, False]:
+        for input_scale in ['normalize_mean_std', 'times_mil', 'identity']:
+            runner(
+                exp_folder, exp,
+                f"--exp_folder={exp_folder} --exp={exp} --run={run} --lin_scale={lin_scale} --input_scale{input_scale}", 
+                run
+                )
+            run += 1
