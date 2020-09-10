cfce76cb6480bef7be689a40260b8c4201993fed
Petr Houška <houskape@gmail.com>
2020-09-10 23:22:27 +0200

    Update params for exp3/bs4_exp5|6

--- experiments/experiments_3/bs4_exp5_runner.py
+++ experiments/experiments_3/bs4_exp5_runner.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     runner = urun.get_runner(sys.argv[1])
     run = 0
     for hidden_lt in ['sep']:
-        for c_size in [3,5,15]:
+        for c_size in [3,7,15]:
             for c_filters in [9, 25]:
                 for cd2x in [0.1, 1]:
                     for hidden_t in ['l1', 'l2']: # l1 as described in the original paper
-                        for hidden_s in [0.1,1]:
+                        for hidden_s in [0.01, 0.1, 1]:
                             runner(
                                 exp_folder, exp,
                                 f"--exp_folder={exp_folder} --exp={exp} --run={run} --c_size={c_size} --c_filters={c_filters} --cd2x={cd2x} --hidden_t={hidden_t} --hidden_s={hidden_s} --hidden_lt={hidden_lt}", 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             for c_filters in [9, 25]:
                 for cd2x in [0.1, 1]:
                     for hidden_t in ['max', 'l2']:
-                        for hidden_s in [0.1, 1]:
+                        for hidden_s in [0.01, 0.1, 1]:
                             runner(
                                 exp_folder, exp,
                                 f"--exp_folder={exp_folder} --exp={exp} --run={run} --c_size={c_size} --c_filters={c_filters} --cd2x={cd2x} --hidden_t={hidden_t} --hidden_s={hidden_s} --hidden_lt={hidden_lt}", 
--- experiments/experiments_3/bs4_exp6_runner.py
+++ experiments/experiments_3/bs4_exp6_runner.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     runner = urun.get_runner(sys.argv[1])
     run = 0
     for c_filters in [12, 24, 48]:
-        for cd2x in [0.1, 1]:
-            for norm2_filt in [0.1, 1]:
-                for l1 in [0.1, 1]:
+        for cd2x in [0.01, 0.1, 1]:
+            for norm2_filt in [0, 0.1, 1]:
+                for l1 in [0.1]:
                     runner(
                         exp_folder, exp,
                         f"--exp_folder={exp_folder} --exp={exp} --run={run} --c_filters={c_filters} --cd2x={cd2x} --norm2_filt={norm2_filt} --l1={l1}", 
